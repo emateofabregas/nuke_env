fa6b10c5fdc0dcaae3e55d31f17977eadc04155a
Eva Mateo Fábregas <hello@emateofabregas.com>
2025-10-06 08:07:06 +0200

    Update menu.py of emTools plugin
    for the 6th of October 2025 version

--- menu.py
+++ menu.py
@@ -1,8 +1,19 @@
 import nuke
 import emTools
-import gizmos
 
 if emTools:
-	print('emTools from 2024 Eva Mateo Fábregas (www.emateofabregas.com)')
+    print("Imported emTools. Last update: 6th of October 2025")
 else:
-	print("emTools not imported")
+    print("Not imported emTools")
+
+if nuke.NUKE_VERSION_MAJOR < 11:
+    # PySide for Nuke up to 10 and other plugins
+    from PySide.QtGui import QPushButton
+    import KnobScripter
+elif nuke.NUKE_VERSION_MAJOR < 16:
+    # PySide2 for default Nuke 11
+    from PySide2.QtWidgets import QPushButton
+    import KnobScripter
+else:
+    # PySide6 for Nuke 16+
+    from PySide6.QtWidgets import QPushButton
