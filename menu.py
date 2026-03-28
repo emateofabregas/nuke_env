122b5faac45e224408e7f7ef0df4cfc0037b66a4
Eva Mateo Fábregas <hello@emateofabregas.com>
2026-03-28 17:15:55 +0100

    Refactor imports and add exception handling
    Updated module imports and error handling.

--- menu.py
+++ menu.py
@@ -1,19 +1,12 @@
 import nuke
-import emTools
+import nukescripts
 
-if emTools:
-    print("Imported emTools. Last update: 6th of October 2025")
-else:
-    print("Not imported emTools")
-
-if nuke.NUKE_VERSION_MAJOR < 11:
-    # PySide for Nuke up to 10 and other plugins
-    from PySide.QtGui import QPushButton
-    import KnobScripter
-elif nuke.NUKE_VERSION_MAJOR < 16:
-    # PySide2 for default Nuke 11
-    from PySide2.QtWidgets import QPushButton
-    import KnobScripter
-else:
-    # PySide6 for Nuke 16+
-    from PySide6.QtWidgets import QPushButton
+## Importing modules
+try:
+	import emTools
+	import emEyesWork
+	import BuddySystem
+	import KnobScripter
+	pass
+except Exception as e:
+	raise e
